f34a0dcf55910b49f58307d294f8dec369f8fe3a
saurabh-jha <saurabh.jha@flipkart.com>
2016-12-17 14:43:41 +0530

    adding models

---
+++ src/main/java/models/Ticket.java
@@ -0,0 +1,46 @@
+package models;
+
+import java.sql.Timestamp;
+
+/**
+ * Created by saurabh.jha on 17/12/16.
+ */
+public class Ticket {
+
+    private String ticketId;
+    private long amount;
+    private Car car;
+    private Timestamp bookingTime;
+
+    public String getTicketId() {
+        return ticketId;
+    }
+
+    public void setTicketId(String ticketId) {
+        this.ticketId = ticketId;
+    }
+
+    public long getAmount() {
+        return amount;
+    }
+
+    public void setAmount(long amount) {
+        this.amount = amount;
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
+
+    public Timestamp getBookingTime() {
+        return bookingTime;
+    }
+
+    public void setBookingTime(Timestamp bookingTime) {
+        this.bookingTime = bookingTime;
+    }
+}
